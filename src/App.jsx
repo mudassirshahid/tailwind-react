655dec3f5217e67b05858299e171cf7da3ccc714
Mudassir <mudassirshahid84@gmail.com>
2024-01-24 19:59:18 +0500

    create landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Navbar from '../src/components/Navbar'
 import { TestimonialOne } from './components/Testimonialone'
+import { LandinngPageOne } from './components/LandingPageOne'
 
 function App() {
   return (
     <>
-    <Navbar />
+    {/* <Navbar />
     <div className='bg-indigo-600 w-full h-screen'>
-    </div>
+    </div> */}
+    <LandinngPageOne />
     </>
   )
 }
